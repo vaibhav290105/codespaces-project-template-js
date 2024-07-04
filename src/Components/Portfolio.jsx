5b293450643a933a79015b72b880654db058e030
vaibhav290105 <130433399+vaibhav290105@users.noreply.github.com>
2024-07-05 00:36:28 +0530

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -33,10 +33,10 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "5 Things To Know About France",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "France, in Western Europe, encompasses medieval cities, alpine villages and Mediterranean beaches",
+    url: "https://www.airalo.com/blog/20-fun-facts-about-france",
   },
   {
     title: "Web Development for Beginners",
@@ -49,12 +49,6 @@ const projectList = [
     description:
       "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
     url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
   },
 ];
 
